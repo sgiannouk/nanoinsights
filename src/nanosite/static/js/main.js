4ec075aea261651d7fa0aec6f7e9312980145377
Stavros Giannoukakos <s.p.giannoukakos@hotmail.com>
2023-10-11 00:52:39 +0300

    Delete src directory

--- src/nanosite/static/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sidebarCollapseButton = document.getElementById('sidebarCollapse');
-    const sidebar = document.querySelector('.sidebar');
-
-    sidebarCollapseButton.addEventListener('click', function () {
-        sidebar.classList.toggle('collapsed');
-    });
-});
